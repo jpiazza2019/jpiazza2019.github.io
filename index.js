6ba1057eaf2614cf335d1e278459881dab8df113
Jack Piazza <jpiazza2019@gmail.com>
2019-05-28 20:14:35 -0400

    Make compass quiz

---
+++ index.js
@@ -0,0 +1,38 @@
+$("#compassQuiz").submit(function(event) {
+  event.preventDefault();
+
+  var answers = $(this).serializeArray();
+
+  var scores = {
+    char1: 0,
+    char2: 0,
+    char3: 0
+  }
+
+  var xVals = {
+    char1: 1,
+    char2: 2,
+    char3: 3
+  }
+
+  var yVals = {
+    char1: 1,
+    char2: 2,
+    char3: 3
+  }
+
+  for (var answer of answers) {
+    scores[answer.value] += 1
+  }
+
+  for(var animal in scores) {
+    sumX += xVals[animal] * scores[animal]
+    sumY += yVals[animal] * scores[animal]
+  }
+
+  finalX = sumX / 3
+  finalY = sumY / 3
+
+  $("#compassQuiz").css('display', 'none');
+  $(".result#"+finalX.toString()+" "+finalY.toString()).css('display', 'block');
+})
